d268a0287d55a303d09617399991a625d0dabe0e
Bruno Okamoto <obrunokenji@gmail.com>
2026-06-08 23:09:11 -0300

    Add usuario Alex

--- database/seeds.sql
+++ database/seeds.sql
@@ -23,7 +23,7 @@ INSERT INTO materias (nome) VALUES
     ('Literatura'),
     ('Redacao');
 
-
-
-
-	
+INSERT INTO usuarios
+    (nome, data_nascimento, email, cpf, telefone, senha_hash)
+VALUES
+    ('Alex', NOW()::DATE, 'a@gmail.com', '11111111111', '111111111', '1')
