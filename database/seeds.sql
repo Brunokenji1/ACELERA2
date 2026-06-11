03a460e40a6f1686cbcad1425de1d47bdde64008
Pedro Chaves <pedroa.chaves10@gmail.com>
2026-06-11 20:56:43 -0300

    update: adição de mais usuarios no seed

--- database/seeds.sql
+++ database/seeds.sql
@@ -26,5 +26,8 @@ INSERT INTO materias (nome) VALUES
 INSERT INTO usuarios
     (nome, data_nascimento, email, cpf, telefone, senha_hash)
 VALUES
-    ('Alex', NOW()::DATE, 'a@gmail.com', '11111111111', '111111111', '1'),
-    ('Convidado', NOW()::DATE, 'convidado@local.com', '22222222222', '222222222', '2')
+    ('Murilo', NOW()::DATE, 'a@gmail.com', '11111111111', '111111111', '1'),
+    ('Bruno', NOW()::DATE, 'convidado@local.com', '22222222222', '222222222', '2'),
+    ('Luis Felipe', NOW()::DATE, 'convidado2@local.com', '33333333333', '333333333', '3')
+    
+
